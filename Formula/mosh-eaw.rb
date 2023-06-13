620815394d81c267f332f285ae13a4b53354e24c
Yoshiaki Kasahara <ykasap@gmail.com>
2023-06-13 11:08:26 +0900

    Sync with the current mosh.rb (abseil.rb and --dirty fixups)

--- Formula/mosh-eaw.rb
+++ Formula/mosh-eaw.rb
@@ -2,13 +2,13 @@ class MoshEaw < Formula
   desc "Remote terminal application with East Asian Width hack"
   homepage "https://mosh.org"
   license "GPL-3.0-or-later"
+
   head "https://github.com/ykasap/mosh.git", branch: "east-asian-width", shallow: false
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
+
   depends_on "pkg-config" => :build
-#  depends_on "tmux" => :build
-#  depends_on "openssl@1.1"
   depends_on "protobuf"
 
   uses_from_macos "ncurses"
@@ -19,12 +19,17 @@ class MoshEaw < Formula
 
     # https://github.com/protocolbuffers/protobuf/issues/9947
     ENV.append_to_cflags "-DNDEBUG"
+    # Keep C++ standard in sync with abseil.rb
+    ENV.append "CXXFLAGS", "-std=c++17"
 
     # teach mosh to locate mosh-client without referring
     # PATH to support launching outside shell e.g. via launcher
-    inreplace "scripts/mosh.pl", "'mosh-client", "\'#{bin}/mosh-client"
+    inreplace "scripts/mosh.pl", "'mosh-client", "'#{bin}/mosh-client"
 
+    # Prevent mosh from reporting `-dirty` in the version string.
+    inreplace "Makefile.am", "--dirty", "--dirty=-Homebrew"
     system "./autogen.sh"
+
     system "./configure", "--prefix=#{prefix}", "--enable-completion"
     system "make", "install"
   end
