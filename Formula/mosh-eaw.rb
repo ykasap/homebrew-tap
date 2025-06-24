f28b3200d4284544f337c1424c98f3fcedffb814
Yoshiaki Kasahara <ykasap@gmail.com>
2025-06-24 10:47:49 +0900

    Update according to the current mosh.rb from Homebrew official repository

--- Formula/mosh-eaw.rb
+++ Formula/mosh-eaw.rb
@@ -1,7 +1,6 @@
 class MoshEaw < Formula
   desc "Remote terminal application with East Asian Width hack"
   homepage "https://mosh.org"
-  license "GPL-3.0-or-later"
 
   head "https://github.com/ykasap/mosh.git", branch: "east-asian-width", shallow: false
 
@@ -11,33 +10,22 @@ class MoshEaw < Formula
   depends_on "pkg-config" => :build
   depends_on "protobuf"
 
-  on_macos do
-    depends_on "tmux" => :build # for `make check`
-  end
+  option "with-test", "Run build-time tests"
 
-  uses_from_macos "ncurses"
-  uses_from_macos "zlib"
+  deprecated_option "without-check" => "without-test"
+
+  depends_on "pkg-config" => :build
+  depends_on "protobuf"
+  depends_on "tmux" => :build if build.with?("test") || build.bottle?
 
   def install
-#    ENV.cxx11
-
-   # https://github.com/protocolbuffers/protobuf/issues/9947
-    ENV.append_to_cflags "-DNDEBUG"
-    # Avoid over-linkage to `abseil`.
-    ENV.append "LDFLAGS", "-Wl,-dead_strip_dylibs" if OS.mac?
-
     # teach mosh to locate mosh-client without referring
     # PATH to support launching outside shell e.g. via launcher
-    inreplace "scripts/mosh.pl", "'mosh-client", "'#{bin}/mosh-client"
+    inreplace "scripts/mosh.pl", "'mosh-client", "\'#{bin}/mosh-client"
 
-    # Prevent mosh from reporting `-dirty` in the version string.
-    inreplace "Makefile.am", "--dirty", "--dirty=-Homebrew"
-    system "./autogen.sh"
-
-    # `configure` does not recognise `--disable-debug` in `std_configure_args`.
-    system "./configure", "--prefix=#{prefix}", "--enable-completion", "--disable-silent-rules"
-    # Mosh provides remote shell access, so let's run the tests to avoid shipping an insecure build.
-    system "make", "check" if OS.mac? # Fails on Linux.
+    system "./autogen.sh" if build.head?
+    system "./configure", "--prefix=#{prefix}", "--enable-completion"
+    system "make", "check" if build.with?("test") || build.bottle?
     system "make", "install"
   end
 
