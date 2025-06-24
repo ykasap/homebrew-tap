a6a04eee0bf4b585e8115c0a79c5e0cc5ccdc511
Yoshiaki Kasahara <ykasap@gmail.com>
2025-06-24 13:32:05 +0900

    Update again (referred a wrong upstream)

--- Formula/mosh-eaw.rb
+++ Formula/mosh-eaw.rb
@@ -2,6 +2,8 @@ class MoshEaw < Formula
   desc "Remote terminal application with East Asian Width hack"
   homepage "https://mosh.org"
 
+  no_autobump! because: :requires_manual_review
+
   head "https://github.com/ykasap/mosh.git", branch: "east-asian-width", shallow: false
 
   depends_on "autoconf" => :build
@@ -10,22 +12,41 @@ class MoshEaw < Formula
   depends_on "pkg-config" => :build
   depends_on "protobuf"
 
-  option "with-test", "Run build-time tests"
+  uses_from_macos "ncurses"
+  uses_from_macos "zlib"
 
-  deprecated_option "without-check" => "without-test"
+  on_macos do
+    depends_on "tmux" => :build # for `make check`
+  end
 
-  depends_on "pkg-config" => :build
-  depends_on "protobuf"
-  depends_on "tmux" => :build if build.with?("test") || build.bottle?
+  on_linux do
+    depends_on "openssl@3" # Uses CommonCrypto on macOS
+  end
 
   def install
+    # https://github.com/protocolbuffers/protobuf/issues/9947
+    ENV.append_to_cflags "-DNDEBUG"
+    # Avoid over-linkage to `abseil`.
+    ENV.append "LDFLAGS", "-Wl,-dead_strip_dylibs" if OS.mac?
+
     # teach mosh to locate mosh-client without referring
     # PATH to support launching outside shell e.g. via launcher
-    inreplace "scripts/mosh.pl", "'mosh-client", "\'#{bin}/mosh-client"
+    inreplace "scripts/mosh.pl", "'mosh-client", "'#{bin}/mosh-client"
 
-    system "./autogen.sh" if build.head?
-    system "./configure", "--prefix=#{prefix}", "--enable-completion"
-    system "make", "check" if build.with?("test") || build.bottle?
+    if build.head?
+      # Prevent mosh from reporting `-dirty` in the version string.
+      inreplace "Makefile.am", "--dirty", "--dirty=-Homebrew"
+      system "./autogen.sh"
+    elsif version <= "1.4.0" # remove `elsif` block and `else` at version bump.
+      # Keep C++ standard in sync with abseil.rb.
+      # Use `gnu++17` since Mosh allows use of GNU extensions (-std=gnu++11).
+      ENV.append "CXXFLAGS", "-std=gnu++17"
+    else # Remove `else` block at version bump.
+      odie "Install method needs updating!"
+    end
+
+    # `configure` does not recognise `--disable-debug` in `std_configure_args`.
+    system "./configure", "--prefix=#{prefix}", "--enable-completion", "--disable-silent-rules"
     system "make", "install"
   end
 
